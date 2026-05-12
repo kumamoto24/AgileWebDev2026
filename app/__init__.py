628e3eb96fccc26960655ae8112e863a54c75068
Shakeel <24370415@student.uwa.edu.au>
2026-05-12 16:46:52 +0800

    Imported loginmanager and created function load user

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,5 +12,15 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"  # where to redirect if not logged in
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models import User
+    return User.query.get(int(user_id))
+
 
 from app import routes,models,sockets
